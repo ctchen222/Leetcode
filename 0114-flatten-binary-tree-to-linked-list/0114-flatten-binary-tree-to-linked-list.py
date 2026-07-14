193d369020b4f53d6c3999e1da46927b13e46ecf
CTCHEN <abfa762466@gmail.com>
2026-07-14 13:47:22 +0800

    Time: 0 ms (100%), Space: 12.6 MB (78.23%) - LeetHub

--- 0114-flatten-binary-tree-to-linked-list/0114-flatten-binary-tree-to-linked-list.py
+++ 0114-flatten-binary-tree-to-linked-list/0114-flatten-binary-tree-to-linked-list.py
@@ -5,20 +5,38 @@
 #         self.left = left
 #         self.right = right
 class Solution(object):
+    # def flatten(self, root):
+    #     """
+    #     :type root: Optional[TreeNode]
+    #     :rtype: None Do not return anything, modify root in-place instead.
+    #     """
+    #     # extra space version
+    #     # time: O(n)
+    #     # space: O(n)
+    #     nodes = []
+    #     def preorder(node):
+    #         if not node: return
+    #         nodes.append(node)
+    #         preorder(node.left)
+    #         preorder(node.right)
+        
+    #     preorder(root)
+    #     for i in range(len(nodes) - 1):
+    #         nodes[i].left = None
+    #         nodes[i].right = nodes[i + 1]
     def flatten(self, root):
         """
         :type root: Optional[TreeNode]
         :rtype: None Do not return anything, modify root in-place instead.
         """
-        nodes = []
-        def preorder(node):
-            if not node: return
-            nodes.append(node)
-            preorder(node.left)
-            preorder(node.right)
-        
-        preorder(root)
-        for i in range(len(nodes) - 1):
-            nodes[i].left = None
-            nodes[i].right = nodes[i + 1]
-        
+        current = root
+        while current:
+            if current.left:
+                cursor = current.left
+                while cursor.right:
+                    cursor = cursor.right
+                cursor.right = current.right
+                current.right = current.left
+                current.left = None
+            current = current.right
+                    
