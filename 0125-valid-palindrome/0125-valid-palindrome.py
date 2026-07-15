ae3a2adc74389a710a09c599f6e5b5407a273c2f
CTCHEN <abfa762466@gmail.com>
2026-07-15 10:26:07 +0800

    Time: 10 ms (93.25%), Space: 13.9 MB (26.28%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.py
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -1,6 +1,8 @@
-import re
-
-class Solution:
-    def isPalindrome(self, s: str) -> bool:
+class Solution(object):
+    def isPalindrome(self, s):
+        """
+        :type s: str
+        :rtype: bool
+        """
         cleaned_string = re.sub(r'[^a-zA-Z0-9]', '', s).lower()
         return cleaned_string == cleaned_string[::-1]
