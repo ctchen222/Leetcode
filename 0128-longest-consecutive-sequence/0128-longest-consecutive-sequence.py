136c2ac5ed7e5f483c73b6b572a008156401ac36
CTCHEN <abfa762466@gmail.com>
2026-09-10 20:37:31 -0700

    feat: longest consucutive seq time & space complexity & test

--- 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.py
+++ 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.py
@@ -19,3 +19,13 @@ class Solution(object):
                     next_num += 1
                 longest = max(longest, consecutive)
         return longest
+
+# Time Complexty: O(n)
+# -> Nested While loop inside for loop runs at most n times in total. When it happened, rest of the numbers will be skipped in the for loop. -> total time complexity is O(n)
+# Space Complexity: O(n)
+
+sol = Solution()
+
+assert(sol.longestConsecutive([100,4,200,1,3,2]) == 4)
+assert(sol.longestConsecutive([0,3,7,2,5,8,4,6,0,1]) == 9)
+assert(sol.longestConsecutive([1,0,1,2]) == 3)
